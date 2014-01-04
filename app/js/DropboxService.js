c1f3a08cdd44eebc80033ced03a2175f7befd4e7
Tim Green <iamtimgreen@gmail.com>
2014-01-05 00:24:59 +1100

    Test add word.

--- app/js/DropboxService.js
+++ app/js/DropboxService.js
@@ -8,11 +8,7 @@ goog.provide('recite.DropboxService');
 recite.DropboxService = function() {
   var DROPBOX_APP_KEY = 'whmi318p8xpr56a';
   this.client_ = new Dropbox.Client({key: DROPBOX_APP_KEY});
-  this.client_.authenticate({interactive: false}, function(error) {
-    if (error) {
-      console.debug('Authentication error: ' + error);
-    }
-  });
+  this.init_();
 };
 
 
@@ -28,4 +24,41 @@ recite.DropboxService.prototype.client_ = null;
  */
 recite.DropboxService.prototype.getClient = function() {
   return this.client_;
+};
+
+
+/**
+ * @private
+ */
+recite.DropboxService.prototype.init_ = function() {
+  this.client_.authenticate({interactive: false}, function(error) {
+    if (error) {
+      console.debug('Authentication error: ' + error);
+    }
+  });
+  var self = this;
+  this.client_.getDatastoreManager().openDefaultDatastore(function(error, datastore) {
+    console.log('datastore ready');
+    if (error) {
+      console.error('Error opening default datastore:', error);
+    }
+
+    self.wordTable_ = datastore.getTable('words');
+    // Ensure that future changes update the list.
+    //datastore.recordsChanged.addListener(updateList);
+    console.log('table ready');
+  });
+};
+
+
+/**
+ * Add a new word.
+ *
+ * @param {string} word new word.
+ */
+recite.DropboxService.prototype.addWord = function(word) {
+  this.wordTable_.insert({
+    'word': word,
+    'created': goog.now()
+  });
 };
--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 goog.provide('recite.App');
 
+goog.require('goog.dom');
+goog.require('goog.events');
 goog.require('recite.DropboxService');
 
 
@@ -12,5 +14,14 @@ recite.App.main = function() {
   if (!dropbox.getClient().isAuthenticated()) {
     dropbox.getClient().authenticate();
   }
+
+  goog.events.listen(goog.dom.getElement('add-word-btn'), 'click', function() {
+    var input = goog.dom.getElement('add-word-input');
+    var word = input.value;
+    input.value = '';
+    dropbox.addWord(word);
+    console.log('add word', word);
+  });
+
 };
 goog.exportSymbol('recite.App.main', recite.App.main);
