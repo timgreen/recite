c7009a8940041a05e409ff06346f4fa8c98e302d
Tim Green <iamtimgreen@gmail.com>
2014-01-04 17:12:37 +1100

    Use connect for dev server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,27 @@ module.exports = function(grunt) {
           }
         }
       }
+    },
+    server: {
+      dev: {
+        port: 9000,
+        alias: [
+          {route: '/', path: 'dest/dev/'},
+          {route: '/assets/js/angular/', path: 'app/bower_components/angular/'},
+          {route: '/assets/js/goog/', path: 'app/bower_components/closure-library/closure/goog/'}
+        ],
+      }
     }
+  });
+
+  grunt.registerMultiTask('server', 'Static server', function() {
+    var done = this.async();
+    var connect = require('connect');
+    var app = connect();
+    this.data.alias.forEach(function(a) {
+      app.use(a.route, connect.static(a.path));
+    });
+    app.listen(this.data.port);
+    grunt.log.write('Server available on http://localhost:9000\nWaiting forever...\n');
   });
 };
