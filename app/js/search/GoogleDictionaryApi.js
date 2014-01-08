46cf85b9276562943643270bb27bf3806a44919e
Tim Green <iamtimgreen@gmail.com>
2014-01-08 17:52:09 +1100

    Use search result type in ctrl.

--- app/js/search/GoogleDictionaryApi.js
+++ app/js/search/GoogleDictionaryApi.js
@@ -1,5 +1,9 @@
 goog.provide('recite.search.GoogleDictionaryApi');
+goog.provide('recite.search.GoogleDictionaryResult');
+
+goog.require('goog.array');
 goog.require('goog.net.Jsonp');
+goog.require('recite.search.SearchResult');
 
 
 
@@ -19,10 +23,33 @@ goog.addSingletonGetter(recite.search.GoogleDictionaryApi);
  */
 recite.search.GoogleDictionaryApi.prototype.search = function(word, callback) {
   var jsonp = new goog.net.Jsonp(new goog.Uri('http://www.google.com/dictionary/json'));
-  var payload = { 'foo': 1, 'bar': true };
   jsonp.send({
     'q': word,
     'sl': 'en',
     'tl': 'en'
-  }, callback);
+  }, function(reply) {
+    var result = new recite.search.GoogleDictionaryResult(reply);
+    callback(result);
+  });
+};
+
+
+
+/**
+ * @constructor
+ * @extends {recite.search.SearchResult}
+ *
+ * @param {Object} json json result from google dictionary api.
+ */
+recite.search.GoogleDictionaryResult = function(json) {
+  goog.base(this, json['query'], json);
+};
+goog.inherits(recite.search.GoogleDictionaryResult, recite.search.SearchResult);
+
+
+/**
+ * @override
+ */
+recite.search.GoogleDictionaryResult.prototype.getType = function() {
+  return 'google-dictionary';
 };
--- app/js/search/SearchCtrl.js
+++ app/js/search/SearchCtrl.js
@@ -1,4 +1,7 @@
 goog.provide('recite.search.SearchCtrl');
+
+goog.require('goog.format.JsonPrettyPrinter');
+goog.require('goog.format.JsonPrettyPrinter.TextDelimiters');
 goog.require('recite.search.GoogleDictionaryApi');
 
 
@@ -9,12 +12,15 @@ goog.require('recite.search.GoogleDictionaryApi');
  */
 recite.search.SearchCtrl = function($scope) {
   $scope.searchWordInput = '';
+  $scope.ouptut = '';
   $scope.searchWord = function() {
-    console.log('ok', $scope.searchWordInput);
     recite.search.GoogleDictionaryApi.getInstance().search($scope.searchWordInput, function(reply) {
-      console.log(reply);
       $scope.$apply(function() {
         $scope.searchWordInput = '';
+        var printer = new goog.format.JsonPrettyPrinter(
+            new goog.format.JsonPrettyPrinter.TextDelimiters());
+        $scope.result = reply;
+        $scope.output = printer.format(reply);
       });
     });
   };
