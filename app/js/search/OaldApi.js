d25901af3f2f0f66d90ba940d7317f69d786a7e1
Tim Green <iamtimgreen@gmail.com>
2014-01-25 23:39:48 +1100

    Parse oald wip.

--- app/js/search/OaldApi.js
+++ app/js/search/OaldApi.js
@@ -4,6 +4,7 @@ goog.provide('recite.search.OaldResult');
 goog.require('goog.Uri');
 goog.require('goog.Uri.QueryData');
 goog.require('goog.dom');
+goog.require('goog.dom.xml');
 goog.require('goog.net.XhrIo');
 goog.require('recite.search.SearchApi');
 goog.require('recite.search.SearchResult');
@@ -34,9 +35,57 @@ recite.search.OaldApi.prototype.search = function(query, callback) {
   }));
   goog.net.XhrIo.send(uri, function(e) {
     var xhr = /** @type {goog.net.XhrIo} */ (e.target);
-    console.log(xhr.getResponseText());
-    var dom = /** @type {Document} */ (goog.dom.htmlToDocumentFragment(xhr.getResponseText()));
-    var entry = goog.dom.getElement('entryContent', dom);
-    console.log(entry);
+    var entryContent = recite.search.OaldApi.createResult(xhr.getResponseText());
+    console.log(entryContent);
+    var dom = /** @type {DocumentFragment} */ (goog.dom.htmlToDocumentFragment(entryContent));
+    console.log(dom);
   });
+};
+
+
+/**
+ * Create result.
+ *
+ * @param {string} htmlText html.
+ * @return {*}
+ */
+recite.search.OaldApi.createResult = function(htmlText) {
+  var start = '<div id="entryContent">';
+  var end = '<!-- End of DIV entry-->';
+  if (htmlText.indexOf(start) != -1) {
+    var tmp = htmlText.substr(htmlText.indexOf(start) + start.length);
+    var entryContent = tmp.substring(0, tmp.indexOf(end));
+    return recite.search.OaldApi.createWord(entryContent);
+  }
+};
+
+
+/**
+ * Create word.
+ *
+ * @param {string} htmlText html.
+ * @return {*}
+ */
+recite.search.OaldApi.createWord = function(htmlText) {
+  var replaceImages = [
+    {
+      from: 'http://oald8.oxfordlearnersdictionaries.com/external/images/coresym.gif',
+      to: 'assets/images/oald/coresym.gif'
+    },
+    {
+      from: 'http://oald8.oxfordlearnersdictionaries.com/external/images/pron-uk.gif',
+      to: 'assets/images/oald/pron-uk.gif'
+    },
+    {
+      from: 'http://oald8.oxfordlearnersdictionaries.com/external/images/pron-us.gif',
+      to: 'assets/images/oald/pron-us.gif'
+    }
+  ];
+  replaceImages.forEach(function(replace) {
+    htmlText = htmlText.replace(new RegExp(replace.from, 'g'), replace.to);
+  });
+
+  console.log('xml', goog.dom.xml.loadXml(htmlText));
+  // TODO(timgreen): new word
+  return htmlText;
 };
